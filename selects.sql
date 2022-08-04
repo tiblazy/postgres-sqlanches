e27e85dcdfb737f8355840661328eb75ea8f93e8
tiblazy <hauntedapply@gmail.com>
2022-08-04 13:50:50 -0300

    fix: select

--- selects.sql
+++ selects.sql
@@ -17,7 +17,7 @@ JOIN produtos_pedidos prod_ped ON prod_ped.pedido_id = ped.id
 JOIN produtos prod ON prod_ped.produto_id = prod.id
 WHERE prod.nome = 'Fritas';
 -- 4)
-SELECT SUM(prod.preco) FROM clientes cli
+SELECT SUM(prod.preço) FROM clientes cli
 JOIN pedidos ped ON cli.id = ped.cliente_id
 JOIN produtos_pedidos prod_ped ON prod_ped.pedido_id = ped.id
 JOIN produtos prod ON prod_ped.produto_id = prod.id
